36b86eef426c8d4aec63fe642bcc3c6115278feb
devendraprajapat12345 <devendraprajapat3086@gmial.com>
2026-02-24 12:08:59 +0530

    remove unnecessary console

--- src/services/mailer/mail.service.js
+++ src/services/mailer/mail.service.js
@@ -1,10 +1,6 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-
-
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
