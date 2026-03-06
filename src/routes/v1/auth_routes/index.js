90ee4185c3849e60179aad2047741c234b9de319
devendraprajapat12345 <devendraprajapat3086@gmial.com>
2026-03-06 21:41:33 +0530

    TEN-33 forgot and reset password api

--- src/routes/v1/auth_routes/index.js
+++ src/routes/v1/auth_routes/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { validate } from "../../../middleware/validate.middleware.js";
-import { loginSchema, registerSchema,resendOtpSchema,verifyOtpSchema} from "./schema.js";
-import { register,login, verifyOTP,resendOTP, verifyToken} from "../../../controllers/v1/auth.controller.js";
+import { loginSchema, registerSchema,emailSchema,verifyOtpSchema, resetPasswordSchema} from "./schema.js";
+import { register,login, verifyOTP,resendOTP, verifyToken, forgotPassword, resetPassword} from "../../../controllers/v1/auth.controller.js";
 import { auth } from "../../../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -10,6 +10,8 @@ router.post("/register",validate(registerSchema),register);
 router.post("/login",validate(loginSchema),login)
 router.get("/verify-token",auth,verifyToken);
 router.post("/verifyOtp", auth , validate(verifyOtpSchema),verifyOTP)
-router.post("/resendOtp", auth , validate(resendOtpSchema),resendOTP);
+router.post("/resendOtp", auth , validate(emailSchema),resendOTP);
+router.post("/forgot-password",validate(emailSchema),forgotPassword);
+router.post("/reset-password",validate(resetPasswordSchema),resetPassword);
 
 export default router;
--- src/routes/v1/auth_routes/schema.js
+++ src/routes/v1/auth_routes/schema.js
@@ -1,72 +1,51 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
- name: Joi.string()
-  .trim()
-  .min(2)
-  .max(50)
-  .pattern(/^[A-Za-z\s]+$/) 
-  .required()
-  .messages({
-    "string.empty": "Name is required",
-    "string.min": "Name must be at least 2 characters",
-    "string.pattern.base": "Name must contain only alphabets",
-  }),
-
-  email: Joi.string()
-    .email()
-    .required()
-    .messages({
-      "string.email": "Invalid email format",
-      "string.empty": "Email is required",
-    }),
-
-  password: Joi.string()
-    .min(8)
-    .max(20)
-    .required()
-    .messages({
-      "string.min": "Password must be at least 8 characters",
-    }),
-
-  organizationName: Joi.string()
+  name: Joi.string()
     .trim()
     .min(2)
-    .max(100)
+    .max(50)
+    .pattern(/^[A-Za-z\s]+$/)
     .required()
     .messages({
-      "string.empty": "Organization name is required",
+      "string.empty": "Name is required",
+      "string.min": "Name must be at least 2 characters",
+      "string.pattern.base": "Name must contain only alphabets",
     }),
+
+  email: Joi.string().email().required().messages({
+    "string.email": "Invalid email format",
+    "string.empty": "Email is required",
+  }),
+
+  password: Joi.string().min(8).max(20).required().messages({
+    "string.min": "Password must be at least 8 characters",
+  }),
+
+  organizationName: Joi.string().trim().min(2).max(100).required().messages({
+    "string.empty": "Organization name is required",
+  }),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .required()
-    .messages({
-      "string.email": "Invalid email format",
-      "string.empty": "Email is required",
-      "any.required": "Email is required",
-    }),
+  email: Joi.string().email().required().messages({
+    "string.email": "Invalid email format",
+    "string.empty": "Email is required",
+    "any.required": "Email is required",
+  }),
 
-  password: Joi.string()
-    .min(6)
-    .required()
-    .messages({
-      "string.empty": "Password is required",
-      "any.required": "Password is required",
-      "string.min": "Password must be at least 6 characters long",
-    }),
+  password: Joi.string().min(6).required().messages({
+    "string.empty": "Password is required",
+    "any.required": "Password is required",
+    "string.min": "Password must be at least 6 characters long",
+  }),
 });
 
 export const verifyOtpSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .required()
-    .messages({
-      "string.email": "Invalid email format",
-      "string.empty": "Email is required",
-    }),
+  email: Joi.string().email().required().messages({
+    "string.email": "Invalid email format",
+    "string.empty": "Email is required",
+  }),
 
   otp: Joi.string()
     .length(6)
@@ -79,9 +58,30 @@ export const verifyOtpSchema = Joi.object({
     }),
 });
 
-export const resendOtpSchema = Joi.object({
+export const resetPasswordSchema = Joi.object({
+  token: Joi.string().trim().required().messages({
+    "string.empty": "Token is required",
+    "any.required": "Token is required",
+  }),
+
+  newPassword: Joi.string().min(6).max(50).required().messages({
+    "string.empty": "New password is required",
+    "string.min": "Password must be at least 6 characters",
+    "string.max": "Password must not exceed 50 characters",
+    "any.required": "New password is required",
+  }),
+
+  confirmPassword: Joi.any().valid(Joi.ref("newPassword")).required().messages({
+    "any.only": "Passwords do not match",
+    "any.required": "Confirm password is required",
+  }),
+});
+
+export const emailSchema = Joi.object({
   email: Joi.string()
     .email()
+    .trim()
+    .lowercase()
     .required()
     .messages({
       "string.email": "Invalid email format",
@@ -89,4 +89,3 @@ export const resendOtpSchema = Joi.object({
       "any.required": "Email is required",
     }),
 });
-
